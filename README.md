8ee5f0551bcfd1a4bbad6ec35badad134b7fcbf9
jaiprakash <p.jaiprakash1994@gmail.com>
2022-11-16 18:59:54 -0500

    Update the Readme file with full steps

--- README.md
+++ README.md
@@ -2,31 +2,54 @@
 
 ## Prerequisites
 
-HTML,JavaScript, Node and NPM.
+HTML,browser, Node and NPM, MongoDb,MongoDb Compass
 [Node](http://nodejs.org/) and [NPM](https://npmjs.org/)
+
+## purpose 
+This project is an  Node.js platform based user form which will be used for Online shopping prototype
 
 ## Steps
 
+### 1. Install MongoDb
+```sh
+below Mentioned link will land you to the page where we can download  
+https://www.mongodb.com/try/download/community
+once we install it we use MongoDb Compass which is an UI for MongoDb 
+https://www.mongodb.com/try/download/compass
+select Download verison not the CLI
+```
+### 2. Create an Database name assigment3
+```sh
+As I have created an Database with name Assigment3 and using it in my project 
+```
+```sh
+### Now pulling the Code from Git Repo 
 ### 1. Clone the source code
+```
 ```sh
 $ git clone https://github.com/jaiprakashpeela/projectdevelopment.git
-$ cd PROG8540_final_exam
+$ cd website
 ```
-### 2. Install Node Modules
+### 1. Install Node Modules
 ```sh
-$ npm install 
+$ npm install
 ```
-### 3. Build the Project
+### 2. Build the Project
 ```sh
 $ npm run build 
 ```
-### 4. Run Test
+### 3. Run Test
 ```sh
 $ npm run test
 ```
-### 5. Start
+### 4. Start
 ```sh
 $ npm start server
 ```
+### 5. open the link in any browser 
+```sh
+http://localhost:8080/
+```
+
 ## Why to use GNU License?
 GNU license is the open source software license and free. Since we are using NodeJS. It allows us to use code in personal development. Also I can generate an patent .we cannot delete the copyright notice and original license from anyone version of the code.
---
+++ website/index.js
@@ -0,0 +1,151 @@
+// import all the modules you need
+const express = require('express');
+const path = require('path');
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser')
+const {check, validationResult} = require('express-validator');
+const { Console } = require("console");
+const { match } = require('assert');
+// database conection
+mongoose.connect("mongodb://localhost:27017/assignment4", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// set up the model for one Order
+const Order = mongoose.model("Order", {
+  Name: String,
+  Email: String,
+  Phone: Number,
+  Address: String,
+  Iphone12: Number,
+  MacBookM2 : Number, 
+  AirpodGen2 : Number, 
+  Tax: Number ,
+  Totalcost: Number
+ });
+var myApp = express();
+
+// set up default location for files 
+myApp.use(express.urlencoded({extended:false}));
+myApp.set('view engine', 'ejs');
+myApp.set('views', path.join(__dirname, 'views')); // set a value for express
+myApp.use(express.static(__dirname + '/public')); // set up a middleware to server static files
+myApp.get('/',function(req, res){
+    res.render('form')
+})
+
+// handle post
+myApp.post('/', [
+    check('name', 'Name is required').notEmpty(),
+    check('address','Address cannot be blank').notEmpty(),
+    check('city','Enter a City').notEmpty(),
+    check('province','Select a Province').notEmpty(),
+    check('phone', 'InValid Phone Number')
+    .notEmpty()
+    .matches(/^\d{10}$/),
+    check('email', 'InValid Email Format').isEmail()
+
+], function(req, res){
+
+    const errors = validationResult(req);
+    if(errors.isEmpty()){ // if no errors are there
+        var iphoneqty = req.body.iphoneqty;
+        var macbookqty = req.body.macbookqty;
+        var airpodqty = req.body.airpodqty;
+        var province = req.body.province;
+        const onatrioTax = 0.13 ;
+        const britishColumbiaTax = 0.05;
+        const quebecTax= 0.05;
+        const newBrunswickTax=0.15;
+        var totalCost = iphoneqty * 500 + macbookqty * 1000 + airpodqty * 100;
+        var tax=0;
+        // tax calculations
+        switch (province)
+        {
+            case "ON" :
+                tax = totalCost*onatrioTax;
+                totalCost+= tax;
+                break;
+            case "BC":
+                tax = totalCost*britishColumbiaTax;
+                totalCost+= tax;
+                break;  
+            case "NB":
+                tax = totalCost*newBrunswickTax;
+                totalCost+= tax;
+                break;
+            case "QU":
+                tax = totalCost*quebecTax;
+                totalCost+= tax;
+                break;
+            default :
+                totalCost+= totalCost*0;
+            }
+        if(totalCost == 0){
+            var itemsList ={
+                'recipt' : false     
+            }
+            res.render('form', { itemsList})
+        }
+        else
+        {
+            // prepare data to send to the view
+            
+            var pageData = {
+                'recipt': true,
+                'totalCost' : totalCost,
+                'tax' : tax
+            }
+            if(iphoneqty>0)
+            {
+                pageData.iphone = iphoneqty * 500;
+            }
+            else{pageData.iphone = 0;}
+            if(macbookqty>0)
+            {
+                pageData.macbook = macbookqty * 1000;
+            }else{pageData.macbook = 0;}
+            if(airpodqty>0)
+            {
+                pageData.airpod = airpodqty * 100;
+            }else{pageData.airpod = 0;}
+            res.render('form', {
+                userdata : req.body,
+                itemsList : pageData
+            });
+        }
+        var dbdata= 
+        {
+            Name: req.body.name, 
+            Email: req.body.email ,
+            Phone: req.body.phone ,
+            Address: (req.body.address +", "+ req.body.city +", " +req.body.province ),
+            Iphone12:  pageData.iphone, 
+            MacBookM2 : pageData.macbook, 
+            AirpodGen2 : pageData.airpod, 
+            Tax: pageData.tax ,
+            Totalcost: pageData.totalCost 
+        }
+        var allOrder = new Order(dbdata);
+      allOrder.save();
+    }
+    else{ // errors display
+        console.log(errors.array());
+        res.render('form', {errors: errors.array()})
+    }
+
+})
+
+// define the route for all orders  page "/"
+myApp.get("/orders", function (req, res) 
+{
+    Order.find({}, function (err, orders) 
+    {
+      res.render("allOrders", { orders: orders });
+    });
+  });
+
+// start the server (listen at a port)
+myApp.listen(8080);
+console.log('Everything executed, open http://localhost:8080/ in the browser.')
